e3d88573f5b1f6546184586853101f4c8b2130cf
fiklehawk <aanishtahir19@gmail.com>
2021-10-26 08:06:26 +0500

    Header Completed. Mobile Friendly Too

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,6 +1,19 @@
+import './Header.scss';
+import logo from "../../logo.svg";
 function Header() {
     return ( 
         <div id="header">
+            <div className="header-container">
+                <div className="logo-container">
+                    <img src={logo} alt="Logo" className="logo" />
+                    <span className="logo-highlight">Reddit</span>
+                    <span>Minimal</span>
+                </div>
+                <div className="search-container">
+                    <input type="text" name="search" id="search" placeholder="Search" />
+                    <button><i class="fas fa-search"></i></button>
+                </div>
+            </div>
         </div>
      );
 }
