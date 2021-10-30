dec7debe4933bad140eb1e2bf9a9a22c7fc21bc5
fiklehawk <aanishtahir19@gmail.com>
2021-10-30 18:58:25 +0500

    added home link to logo

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ import Header from './features/Header/Header.js';
   // Imporitng Content Componet
 import Content from './features/Content/Content.js';
 //---------------//
-import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <BrowserRouter>
+      
         <Content />
-      </BrowserRouter>
       
     </div>
   );
--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,14 +1,21 @@
 import './Header.scss';
 import logo from "../../logo.svg";
+import { Link } from 'react-router-dom';
 function Header() {
     return ( 
         <div id="header">
             <div className="header-container">
+            
                 <div className="logo-container">
+                <Link to="/">
                     <img src={logo} alt="Logo" className="logo" />
                     <span className="logo-highlight">Reddit</span>
                     <span>Minimal</span>
+                    </Link>
                 </div>
+                
+                
+                
                 <div className="search-container">
                     <input type="text" name="search" id="search" placeholder="Search" />
                     <button><i class="fas fa-search"></i></button>
