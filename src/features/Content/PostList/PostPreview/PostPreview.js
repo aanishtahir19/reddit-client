b7b1929b0252948aad3cf2df95ce0d33d9658732
fiklehawk <aanishtahir19@gmail.com>
2021-11-08 13:04:23 +0500

    Added functionality to the search Bar

--- src/features/Content/PostList/PostPreview/PostPreview.js
+++ src/features/Content/PostList/PostPreview/PostPreview.js
@@ -1,32 +1,38 @@
 // Importing Styles
-import "./PostPreview.scss";
+import './PostPreview.scss';
 // Importing Upvotes Component
-import PostUpvotes from "../../../PostUpvotes/PostUpvotes";
+import PostUpvotes from '../../../PostUpvotes/PostUpvotes';
 // Importing Time Passes since postd funciton
-import { timePassedPosted } from "../../../../utils/utils.js";
+import { timePassedPosted } from '../../../../utils/utils.js';
 // Import react skelton
 // import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-import { useState } from "react";
-import Showdown from "showdown";
-import { Markup } from "interweave";
+import 'react-loading-skeleton/dist/skeleton.css';
+import { useState } from 'react';
+import Showdown from 'showdown';
+import { Markup } from 'interweave';
 function PostPreview({ post }) {
   let [data, setData] = useState(null);
   let converter = new Showdown.Converter();
   const imagesrc = () => {
     if (post.preview !== undefined) {
       let src;
-      if (post.preview.images[0].source.url.match("external-preview")) {
+      if (
+        post.preview.images[0].source.url.match('external-preview')
+      ) {
         return null;
       }
-      src = post.preview.images[0].source.url.replace("preview", "i");
+      src = post.preview.images[0].source.url.replace('preview', 'i');
 
       return (
-        <div className="image-container">
+        <div className='image-container'>
           <img
             src={src}
-            alt="nothing"
-            style={{ maxWidth: "100%", borderRadius: "5px", height: "400px" }}
+            alt='nothing'
+            style={{
+              maxWidth: '100%',
+              borderRadius: '5px',
+              height: '400px',
+            }}
           />
         </div>
       );
@@ -41,10 +47,6 @@ function PostPreview({ post }) {
       setData(null);
     } else {
       if (post.selftext) {
-        // setData(post.selftext.split("\n").map((para, index)=> {
-        //   return <div><p key={index}>{para}</p><br/></div>
-        // }));c
-
         setData(converter.makeHtml(post.selftext));
       }
     }
@@ -53,10 +55,16 @@ function PostPreview({ post }) {
     if (post.media_metadata) {
       let data = post.media_metadata;
       return Object.keys(data).map((key) => {
-        if (data[key].status === "valid") {
-          if (data[key].e === "Image") {
-            let url = data[key].s.u.replace("preview", "i");
-            return <img src={url} alt="Image" style={{ padding: "10px" }} />;
+        if (data[key].status === 'valid') {
+          if (data[key].e === 'Image') {
+            let url = data[key].s.u.replace('preview', 'i');
+            return (
+              <img
+                src={url}
+                alt='Image'
+                style={{ padding: '10px' }}
+              />
+            );
           }
         }
       });
@@ -66,15 +74,15 @@ function PostPreview({ post }) {
     console.log(post.secure_media_embed);
   };
   return (
-    <div className="post-preview" onClick={testing}>
+    <div className='post-preview' onClick={testing}>
       {<PostUpvotes upvotes={post.ups} />}
-      <div className="preview-content">
+      <div className='preview-content'>
         <p>{`Posted by u/${post.author} ${timePassed}`}</p>
         <h2>{post.title}</h2>
 
         {/* Post Url */}
-        {post.url && post.url.match("www.reddit.com") === null ? (
-          <a href={post.url} target="_blank">
+        {post.url && post.url.match('www.reddit.com') === null ? (
+          <a href={post.url} target='_blank'>
             {post.url}
           </a>
         ) : null}
@@ -86,7 +94,7 @@ function PostPreview({ post }) {
         {getPostExtraImages()}
 
         {/* Post Text */}
-        <div className="post-text">
+        <div className='post-text'>
           <Markup content={data} />
 
           {/* Reddit Video */}
@@ -94,25 +102,26 @@ function PostPreview({ post }) {
           post.secure_media.reddit_video &&
           post.secure_media.reddit_video.fallback_url ? (
             <iframe
-              clsssName="reddit-video"
-              height="512px"
-              width="100%"
+              clsssName='reddit-video'
+              height='512px'
+              width='100%'
               src={post.secure_media.reddit_video.fallback_url}
               allowFullScreen
             ></iframe>
           ) : null}
           {post.selftext ? (
-            <button onClick={() => togglePostData()}>Show Text</button>
+            <button onClick={() => togglePostData()}>
+              Show Text
+            </button>
           ) : null}
 
           {/* Other Videos or gifs */}
           {post.secure_media_embed.media_domain_url ? (
-            <div id="videos">
+            <div id='videos'>
               <iframe
-                
                 src={post.secure_media_embed.media_domain_url}
-                width="100%"
-                height="200px"
+                width='100%'
+                height='200px'
                 allowFullScreen
               ></iframe>
             </div>
@@ -120,7 +129,7 @@ function PostPreview({ post }) {
         </div>
 
         {/* Extra details */}
-        <div id="extra-details">
+        <div id='extra-details'>
           <p>{timePassed}</p>
         </div>
       </div>
--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,28 +1,49 @@
 import './Header.scss';
-import logo from "../../logo.svg";
+import logo from '../../logo.svg';
 import { Link } from 'react-router-dom';
+import { changeSubreddit } from '../Content/PostList/PostListSlice';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 function Header() {
-    return ( 
-        <div id="header">
-            <div className="header-container">
-            
-                <div className="logo-container">
-                <Link to="/">
-                    <img src={logo} alt="Logo" className="logo" />
-                    <span className="logo-highlight">Reddit</span>
-                    <span>Minimal</span>
-                    </Link>
-                </div>
-                
-                
-                
-                <div className="search-container">
-                    <input type="text" name="search" id="search" placeholder="Search" />
-                    <button><i class="fas fa-search"></i></button>
-                </div>
-            </div>
+  let [searchTerm, setSearchTerm] = useState('');
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    console.log(searchTerm);
+    dispatch(changeSubreddit(searchTerm.toLowerCase()));
+    setSearchTerm('');
+  };
+
+  return (
+    <div id='header'>
+      <div className='header-container'>
+        <div className='logo-container'>
+          <Link to='/'>
+            <img src={logo} alt='Logo' className='logo' />
+            <span className='logo-highlight'>Reddit</span>
+            <span>Minimal</span>
+          </Link>
         </div>
-     );
+
+        <div className='search-container'>
+          <form action='javascript:void(0);'>
+            <input
+              type='text'
+              name='search'
+              id='search'
+              placeholder='Search'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <Link to={`/${searchTerm}/hot`}>
+              <button type='submit' onClick={() => handleClick()}>
+                <i class='fas fa-search'></i>
+              </button>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
